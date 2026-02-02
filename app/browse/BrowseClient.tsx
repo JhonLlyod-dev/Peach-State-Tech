0457178f7ff5b1c569e9b07f0ee62080be1aa17f
jhon-dev <navarro.jhonllyod2005@gmail.com>
2026-02-02 15:14:47 +0800

    added image format

--- app/browse/BrowseClient.tsx
+++ app/browse/BrowseClient.tsx
@@ -149,7 +149,7 @@ export default function BrowseClient() {
         </h2>
 
         <Image
-          src="/search.webp" // same image reused ✅
+          src="/search.webp" 
           alt="  Confused animated character with no articles available"
           width={160}
           height={160}
