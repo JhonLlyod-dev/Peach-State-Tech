1f9fbcb50bb40b8c0f2d28420ad6db3be82ebbf6
jhon-dev <navarro.jhonllyod2005@gmail.com>
2026-02-10 02:10:32 +0800

    added token for sanity

--- sanity/sanityClient.ts
+++ sanity/sanityClient.ts
@@ -3,13 +3,14 @@ import { createImageUrlBuilder } from '@sanity/image-url';
 
 const ID = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-
+const token = process.env.SANITY_API_TOKEN
 
 export const sanityClient = createClient({
   projectId: ID,
   dataset: dataset,
   apiVersion: '2026-01-29',
-  useCdn: true
+  useCdn: false,
+  token
 })
 
 const builder = createImageUrlBuilder(sanityClient)
