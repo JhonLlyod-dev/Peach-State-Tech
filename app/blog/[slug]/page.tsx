bd8e55643716e5db3d0df23586c75bc826642d6e
jhon-dev <navarro.jhonllyod2005@gmail.com>
2026-03-19 22:30:41 +0800

    missed domain spelling

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -101,7 +101,7 @@ export async function generateMetadata({ params }: PageProps): Promise<Metadata>
       ]
     },
     alternates: {
-      canonical: `https://www.peachstatetech.tech/blog/${post.slug}`,
+      canonical: `https://www.peachstate.tech/blog/${post.slug}`,
     },
     keywords: [
       post.title,
