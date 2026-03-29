367c81dc75c30009bc195440a778b2c5447664a8
jhon-dev <navarro.jhonllyod2005@gmail.com>
2026-03-29 23:30:34 +0800

    remove from sitemap 410 page

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -17,7 +17,7 @@ export default {
   sitemapSize: 5000,
   changefreq: 'weekly',
   priority: 0.7,
-  exclude: ['/thankyou', '/thank-you/**'],
+  exclude: ['/thankyou', '/thank-you/**', '/404', '/404/**', '/410', '/410/**'],
 
   additionalPaths: async () => {
     const posts = await sanityClient.fetch(`
