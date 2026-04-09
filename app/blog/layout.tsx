c200c02239a861ea5b29f041a59dbb4752de2651
jhon-dev <navarro.jhonllyod2005@gmail.com>
2026-04-10 01:04:16 +0800

    added new metadata

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blogs | Peach State Tech",
+  description: "Peach State Tech Blogs Category Page",
+}
+
 export default function CategoryLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <>{children}</>;
 }
