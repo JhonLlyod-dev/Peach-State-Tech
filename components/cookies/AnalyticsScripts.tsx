1ccaf706b19a41a419f5c088d48d7e9c34dce251
jhon-dev <navarro.jhonllyod2005@gmail.com>
2026-08-06 20:16:22 +0800

    Setup GA4

--- components/cookies/AnalyticsScripts.tsx
+++ components/cookies/AnalyticsScripts.tsx
@@ -18,6 +18,20 @@ export default function AnalyticsScripts() {
         strategy="afterInteractive"
       />
       <Analytics />
+
+      {/* Google Analytics (gtag.js) */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
     </>
   );
 }
